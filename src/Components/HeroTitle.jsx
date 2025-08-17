e04866d48aad5c362e573f981dc5a58e0b09b16b
MD. RAKIBUL HASAN HEMEL <rhn109820@gmail.com>
2025-08-17 06:46:14 +0600

    Text formated and Updated

--- src/Components/HeroTitle.jsx
+++ src/Components/HeroTitle.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { motion } from "framer-motion";
 
 function HeroTitle() {
   return (
@@ -7,29 +8,32 @@ function HeroTitle() {
       <motion.h1
         initial={{ opacity: 0, y: 80 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: 'easeOut' }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
         className="text-4xl sm:text-6xl font-bold font-poppins leading-tight text-clean dark:text-ground"
       >
-        Hey, I'm{" "}
-        <motion.span className='text-primary'
-        >
-          Hemel
-        </motion.span>{" "}
-        I build websites so good, even your Wi-Fi wants to load faster.
+        Hey, I'm <motion.span className="text-primary">Hemel</motion.span>, a
+        full-stack developer building clean, responsive, and modern websites.
       </motion.h1>
 
       <motion.p
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.9, duration: 0.8, ease: 'easeOut' }}
+        transition={{ delay: 0.9, duration: 0.8, ease: "easeOut" }}
         className="text-base sm:text-lg mt-6 font-outfit font-light max-w-xl mx-auto"
       >
-        “<span className='dark:text-primary'>Frontend Developer</span>  | <span className='text-primary dark:text-ground'>React Enthusiast</span> | <span className='dark:text-primary'>Future Full-Stack Developer</span>”
+        “<span className="dark:text-primary">Full-Stack Developer</span> |{" "}
+        <span className="text-primary dark:text-ground">React Enthusiast</span>{" "}
+        | <span className="dark:text-primary">MERN Specialist</span>”
       </motion.p>
       <motion.p
-      initial={{opacity:0, y:20}}
-      animate={{opacity:1, y:0}}
-      transition={{delay:1.5, duration:0.8, ease: 'easeIn'}} className='font-inter'>“I'm a passionate web developer focused on crafting clean, responsive, and modern websites. Currently deepening my skills in full-stack development.”</motion.p>
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.5, duration: 0.8, ease: "easeIn" }}
+        className="font-inter"
+      >
+        “I'm a passionate full-stack web developer, crafting clean, responsive,
+        and modern websites with a focus on React and the MERN stack.”
+      </motion.p>
     </div>
   );
 }
